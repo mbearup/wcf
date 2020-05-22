4b1b99fd1837dbc7976e632f98145d1993a0d509
mbearup <mbearup@microsoft.com>
2020-05-22 09:51:28 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -16,10 +16,8 @@ if [ ! -d $dest ]; then
 fi
 
 echo "INFO: Installing WCF libraries to $dest"
-lib=bin/Unix.AnyCPU.Debug/System.Private.ServiceModel/System.Private.ServiceModel.dll
-$sudo install -o $owner -g $owner -m 644 ${lib} "${lib::-3}pdb" $dest
 
-libs="System.ServiceModel.Http/System.ServiceModel.Http.dll System.ServiceModel.Duplex/System.ServiceModel.Duplex.dll System.ServiceModel.NetTcp/System.ServiceModel.NetTcp.dll System.ServiceModel.Primitives/System.ServiceModel.Primitives.dll System.ServiceModel.Security/System.ServiceModel.Security.dll"
-for lib in $libs; do
-    $sudo install -o $owner -g $owner -m 644 "bin/AnyOS.AnyCPU.Debug/$lib" "bin/AnyOS.AnyCPU.Debug/${lib::-3}pdb" $dest
+prefixes="System.ServiceModel.Http System.ServiceModel.Duplex System.ServiceModel.NetTcp System.ServiceModel.Primitives System.ServiceModel.Security System.Private.ServiceModel"
+for prefix in $prefixes; do
+    $sudo install -o $owner -g $owner -m 644 "bin/AnyOS.AnyCPU.Debug/$prefix/netstandard/$prefix.dll" $dest
 done
