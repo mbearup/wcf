7ede14b69d41a1708a8c1ccb641c9e5b238960ce
mbearup <mbearup@microsoft.com>
2020-07-27 19:36:17 -0700

    Binplace System.Net.Http.WinHttpHandler.dll and System.Security.Cryptography.ProtectedData.dll

--- install.sh
+++ install.sh
@@ -21,3 +21,8 @@ prefixes="System.ServiceModel.Http System.ServiceModel.Duplex System.ServiceMode
 for prefix in $prefixes; do
     $sudo install -o $owner -g $owner -m 644 "bin/AnyOS.AnyCPU.Debug/$prefix/netstandard/$prefix.dll" $dest
 done
+
+dlls="bin/AnyOS.AnyCPU.Debug/corefx/netstandard/System.Net.Http.WinHttpHandler.dll Tools/dotnetcli/sdk/2.1.2/runtimes/unix/lib/netstandard1.3/System.Security.Cryptography.ProtectedData.dll"
+for dll in $dlls; do
+  $sudo install -o $owner -g $owner -m 644 "${dll}" $dest
+done
